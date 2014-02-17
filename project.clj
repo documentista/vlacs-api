26ab971f81268ad2da55bdf0707995ce993fc8ac
Tom Dolan <tgdolan@gmail.com>
2014-02-17 08:01:27 -0500

    Fixing dependencies.

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
                  [liberator "0.10.0"]
                  [compojure "1.1.3"]
 		 [ring/ring-core "1.2.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]]
-                 [ring-middleware-format "0.3.1"])
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [ring-middleware-format "0.3.1"]])
